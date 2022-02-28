8e0de758492e884dd01cbf32e009a92eec7e7979
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-01 00:19:59 +0100

    added Register route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <div className="body">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
