7303f97516c00b35bbe210b6ad881467bcbab073
Okike Chinonso <Chinonsookike@gmail.com>
2022-03-01 00:17:37 +0100

    finish working signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,23 +1,29 @@
+import { Link } from "react-router-dom";
+
 export default function signup() {
   return (
-    <div>
-      <div class="form-floating mb-3">
-        <input
-          type="email"
-          class="form-control"
-          id="floatingInput"
-          placeholder="name@example.com"
-        />
-        <label for="floatingInput">Email address</label>
-      </div>
-      <div class="form-floating">
-        <input
-          type="password"
-          class="form-control"
-          id="floatingPassword"
-          placeholder="Password"
-        />
-        <label for="floatingPassword">Password</label>
+    <div className="container">
+      <div className="card">
+        <h3 className="logo">PMS</h3>
+        <p className="title">Sign in to your account </p>
+        <div className="form">
+          <label className="email">Email</label>
+          <input className="email-input" type="email" />
+
+          <label className="password">Password</label>
+          <input className="password-input" type="password" />
+
+          <div className="fp-section">
+            <div className="remember">
+              <input type="checkbox" />
+              <p>Remember me</p>
+            </div>
+            <p className="fp">Forgot password?</p>
+          </div>
+          <div className="signin"><p>Sign In</p></div>
+        </div>
+
+        {/* <div className="account">Don’t have an account? Sign up</div> */}
       </div>
     </div>
   );
