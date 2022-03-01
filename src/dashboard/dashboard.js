e821bbe84ccff6a310de158c8ee193471d21b1c3
Okike Chinonso <Chinonsookike@gmail.com>
2022-03-01 18:06:05 +0100

    added country field

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -5,13 +5,19 @@ export default function dashboard() {
       <div className="item1">header</div>
       <div className="item2">advert</div>
       <div className="item3">
-        <h4>System Settings</h4>
-        <p className="profile">Organization Profile</p>
-        <hr />
-        <div className="itemOne">paragraph1</div>
-        <hr />
+        <div>
+          <h4>System Settings</h4>
+          <p className="profile">Organization Profile</p>
+          <hr />
+        </div>
+        <div>
+          <div className="itemOne">paragraph1</div>
+          <hr />
+        </div>
+        <div>
         <p className="vision">Organization Vision</p>
-        <div></div>
+        </div>
+        
         <div className="itemTwo">paragraph2</div>
       </div>
     </div>
