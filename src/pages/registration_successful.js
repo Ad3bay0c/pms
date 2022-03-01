9ad9f582318f19b2450ff974cda38e3ab044f0a0
Okike Chinonso <Chinonsookike@gmail.com>
2022-03-01 11:56:09 +0100

    added the successful info section

--- src/pages/registration_successful.js
+++ src/pages/registration_successful.js
@@ -1,10 +1,14 @@
-import './registration_successful.css';
+import "./registration_successful.css";
 
-export default function successful(){
-    return (
-            <div className="card-successful">
-                <div></div>
-            </div>
-       
-    )
+export default function successful() {
+  return (
+    <div className="card-successful">
+      <div className="tick-section"></div>
+      <div className="successful-section">
+        <p className="awesome">Awesome!</p>
+        <p className="info">Your organization has been successfully created.</p>
+        <div className="goto"><p>Go to Dashboard</p></div>
+      </div>
+    </div>
+  );
 }
