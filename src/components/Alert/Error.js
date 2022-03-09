0c1e9b8c161cc88574475964c984bafcf6c3ee80
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-09 21:46:46 +0100

    got state from redux using connect

--- src/components/Alert/Error.js
+++ src/components/Alert/Error.js
@@ -1,19 +1,25 @@
-import { useContext } from "react";
-import AuthContext from "../../context/Auth/AuthContext";
-const Error = () => {
-  const { errors } = useContext(AuthContext);
-  
-  return errors && (
-    <div
-      className={`alert alert-danger`}
-      style={{ position: "fixed", top: "50px", right: "15px", zIndex: "10000" }}
-    >
-      {Object
-        .keys(errors)
-        .map((key) => errors[key][0])
-        .join("\n")}
-    </div>
+import { connect } from "react-redux";
+const Error = ({ auth: { errors } }) => {
+  return (
+    errors && (
+      <div
+        className={`alert alert-danger`}
+        style={{
+          position: "fixed",
+          top: "50px",
+          right: "15px",
+          zIndex: "10000",
+        }}
+      >
+        {Object.keys(errors)
+          .map((key) => errors[key][0])
+          .join("\n")}
+      </div>
+    )
   );
 };
 
-export default Error;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+export default connect(mapStateToProps)(Error);
