74209433a158d64365f1eb76f2561cf5c95a9c0f
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-09 21:45:45 +0100

    changed from context API to redux

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Home from "./pages/Home";
 import Register from "./pages/Auth/Register";
 import Signin from "./pages/Auth/signin";
 import Successful from "./pages/Auth/registration_successful";
-import AuthState from "./context/Auth/AuthState";
 import Alert from "./components/Alert/Alert";
 import Error from "./components/Alert/Error";
 import PrivateRoute from "./PrivateRoute";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const App = () => {
   return (
     <Router>
-      <AuthState>
+      <Provider store={store}>
         <Alert></Alert>
         <Error></Error>
         <Routes>
@@ -27,7 +28,7 @@ const App = () => {
           <Route path="/login" element={<Signin />} />
           <Route path="/successful" element={<Successful />} />
         </Routes>
-      </AuthState>
+      </Provider>
     </Router>
   );
 };
